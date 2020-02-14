e34c1717340986bbfe20adc00bc207e20bca41c5
Manuel Céspedes <virgiliomca@outlook.com>
2020-02-14 19:27:09 -0300

    Primera protección

---
+++ AppConfigFile/frmPrincipal.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtCadenaConexion = New System.Windows.Forms.TextBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.btnLeer = New System.Windows.Forms.Button()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.lblNombreCadCon = New System.Windows.Forms.Label()
+        Me.txtNombreCadCon = New System.Windows.Forms.TextBox()
+        Me.lblCadenaConexion = New System.Windows.Forms.Label()
+        Me.txtNombresCadCon = New System.Windows.Forms.RichTextBox()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtCadenaConexion
+        '
+        Me.txtCadenaConexion.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtCadenaConexion.Location = New System.Drawing.Point(62, 32)
+        Me.txtCadenaConexion.Name = "txtCadenaConexion"
+        Me.txtCadenaConexion.Size = New System.Drawing.Size(540, 20)
+        Me.txtCadenaConexion.TabIndex = 0
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'btnLeer
+        '
+        Me.btnLeer.Location = New System.Drawing.Point(12, 58)
+        Me.btnLeer.Name = "btnLeer"
+        Me.btnLeer.Size = New System.Drawing.Size(75, 23)
+        Me.btnLeer.TabIndex = 2
+        Me.btnLeer.Text = "Leer"
+        Me.btnLeer.UseVisualStyleBackColor = True
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Location = New System.Drawing.Point(93, 58)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.TabIndex = 3
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'lblNombreCadCon
+        '
+        Me.lblNombreCadCon.AutoSize = True
+        Me.lblNombreCadCon.Location = New System.Drawing.Point(9, 9)
+        Me.lblNombreCadCon.Name = "lblNombreCadCon"
+        Me.lblNombreCadCon.Size = New System.Drawing.Size(322, 13)
+        Me.lblNombreCadCon.TabIndex = 4
+        Me.lblNombreCadCon.Text = "Nombre de la cadena de conexión: AppCopnfigFile.My.MySettings."
+        '
+        'txtNombreCadCon
+        '
+        Me.txtNombreCadCon.Location = New System.Drawing.Point(329, 6)
+        Me.txtNombreCadCon.Name = "txtNombreCadCon"
+        Me.txtNombreCadCon.Size = New System.Drawing.Size(272, 20)
+        Me.txtNombreCadCon.TabIndex = 5
+        Me.txtNombreCadCon.Text = "cadena_conexion"
+        '
+        'lblCadenaConexion
+        '
+        Me.lblCadenaConexion.AutoSize = True
+        Me.lblCadenaConexion.Location = New System.Drawing.Point(9, 35)
+        Me.lblCadenaConexion.Name = "lblCadenaConexion"
+        Me.lblCadenaConexion.Size = New System.Drawing.Size(47, 13)
+        Me.lblCadenaConexion.TabIndex = 6
+        Me.lblCadenaConexion.Text = "Cadena:"
+        '
+        'txtNombresCadCon
+        '
+        Me.txtNombresCadCon.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtNombresCadCon.Location = New System.Drawing.Point(174, 58)
+        Me.txtNombresCadCon.Name = "txtNombresCadCon"
+        Me.txtNombresCadCon.Size = New System.Drawing.Size(428, 115)
+        Me.txtNombresCadCon.TabIndex = 7
+        Me.txtNombresCadCon.Text = ""
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(93, 87)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardar.TabIndex = 8
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'frmPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(614, 185)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.txtNombresCadCon)
+        Me.Controls.Add(Me.lblCadenaConexion)
+        Me.Controls.Add(Me.txtNombreCadCon)
+        Me.Controls.Add(Me.lblNombreCadCon)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Controls.Add(Me.btnLeer)
+        Me.Controls.Add(Me.txtCadenaConexion)
+        Me.Name = "frmPrincipal"
+        Me.Text = "Form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtCadenaConexion As TextBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents btnLeer As Button
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents lblNombreCadCon As Label
+    Friend WithEvents txtNombreCadCon As TextBox
+    Friend WithEvents lblCadenaConexion As Label
+    Friend WithEvents txtNombresCadCon As RichTextBox
+    Friend WithEvents btnGuardar As Button
+End Class
---
+++ AppConfigFile/frmPrincipal.vb
@@ -0,0 +1,75 @@
+﻿Imports System.Configuration
+Public Class frmPrincipal
+    Private Sub BtnLeer_Click(sender As Object, e As EventArgs) Handles btnLeer.Click
+        'Leemos todas las cadenas de conexión en el administrador de configuraciones y carga en un control
+        'También lee la cadena de conexión especificada en el control con el nombre de la cadena de conexión a buscar
+        Try
+            txtNombresCadCon.Text = String.Empty
+            For i As Integer = 0 To ConfigurationManager.ConnectionStrings.Count - 1
+                txtNombresCadCon.Text += ConfigurationManager.ConnectionStrings(i).Name & IIf(i = ConfigurationManager.ConnectionStrings.Count - 1, String.Empty, vbCrLf)
+            Next
+
+            If ConfigurationManager.ConnectionStrings("AppConfigFile.My.MySettings." & txtNombreCadCon.Text) IsNot Nothing Then
+                txtCadenaConexion.Text = ConfigurationManager.ConnectionStrings("AppConfigFile.My.MySettings." & Me.txtNombreCadCon.Text).ConnectionString
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Try
+            AgregarCadenaConexion()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+
+    ' Clear connection strings collection.
+    Shared Sub LimpiarCadenaConexion()
+
+        ' Get the application configuration file.
+        Dim config As System.Configuration.Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+
+        ' Clear the connection strings collection.
+        Dim csSection As ConnectionStringsSection = config.ConnectionStrings
+        csSection.ConnectionStrings.Clear()
+
+        ' Save the configuration file.
+        config.Save(ConfigurationSaveMode.Modified)
+
+        Application.Restart()
+    End Sub
+
+    Private Sub AgregarCadenaConexion()
+        'Si no hay nombre de cadena de conexión
+        If txtNombreCadCon.Text.Trim = "" Then
+            Exit Sub
+        End If
+
+        ' Get the configuration file.
+        Dim config As System.Configuration.Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+        Dim csSection As ConnectionStringsSection = config.ConnectionStrings
+
+        'Si no existe cadena con ese nombre
+        If ConfigurationManager.ConnectionStrings("AppCopnfigFile.My.MySettings." & Me.txtNombreCadCon.Text) Is Nothing Then
+            ' Add the connection string.
+            csSection.ConnectionStrings.Add(New ConnectionStringSettings("AppCopnfigFile.My.MySettings." & Me.txtNombreCadCon.Text, txtCadenaConexion.Text, "System.Data.SqlClient"))
+
+            ' Save the configuration file.
+            config.Save(ConfigurationSaveMode.Modified)
+            Application.Restart()
+        End If
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        Try
+            LimpiarCadenaConexion()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
